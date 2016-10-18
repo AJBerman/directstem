c73daa15b1fd5bd98d5609cd2b3e85f71c5d1abc
shay <shay.nguyen@gmail.com>
2016-10-18 09:44:38 -0700

    Move all graph creation logic to setup file.
    previously, i left some logic in view file by accident
    also remove unused graph directives

--- app/features/graph/joint_setup.js
+++ app/features/graph/joint_setup.js
@@ -9,8 +9,134 @@ var MAIN_GRAPH = new joint.dia.Graph();
 // set it to  an actual fully functional graph.
 var DEFAULT_GRAPH = new joint.dia.Graph();
 
-
 /* =============== CREATING THE DEFAULT GRAPH =============== */
+var linklist = [];
+var source = new joint.shapes.devs.Coupled({
+    position: {x: 50, y: 50},
+    size: {width: 140, height: 70},
+    inPorts: ['In'],
+    outPorts: ['Out'],
+    attrs: {
+        '.label': {
+            text: 'Source', 'data-toggle': 'popover', title: 'Modify Me!',
+            'data-content': '<form action="action_page.php"> \
+                  Type:<br> \
+                  <select name="type"> \
+                    <option value="Service">Service</option> \
+                    <option value="Graph">Graph</option> \
+                  </select><br> \
+                  Service:<br> \
+                  <select name="service"> \
+                    <option value="WeatherByZip">WeatherByZip</option> \
+                    <option value="WeatherByCity">WeatherByCity</option> \
+                    <option value="calc_add">CalculatorAdd</option> \
+                    <option value="calc_sub">CalculatorSubtract</option> \
+                    <option value="calc_mul">CalculatorMultiply</option> \
+                    <option value="calc_div">CalculatorDivide</option> \
+                  </select><br> \
+                  Endpoint:<br> \
+                  <select name="serviceEndpoint"> \
+                    <option value="foo">Foo</option> \
+                    <option value="bar">Bar</option> \
+                  </select><br> \
+                  <input type="submit" value="Save"><button>Edit...</button><button>Delete</button></form>',
+            'data-html': 'true', 'data-container': 'body'
+        },
+        rect: {fill: '#d9534f'},
+
+        '.inPorts circle': {
+            fill: '#16A085',
+            magnet: 'passive',
+            type: 'input',
+            r: '10'
+        },
+        '.outPorts circle': {fill: '#E74C3C', type: 'output', r: '10'}
+    }
+});
+
+var target = source.clone().translate(750, 400).attr('text/text', 'Target');
+
+var link = new joint.dia.Link({
+    source: {
+        id: source.id,
+        port: "Out"
+    },
+    target: {
+        id: target.id,
+        port: "In"
+    },
+    router: {name: 'metro'},
+    connector: {name: 'rounded'},
+    attrs: {
+        '.connection': {
+            stroke: '#333333',
+            'stroke-width': 3
+        },
+        '.marker-target': {
+            fill: '#333333',
+            d: 'M 10 0 L 0 5 L 10 10 z'
+        }
+    }
+});
+linklist.push(link);
+
+var obstacle = source.clone().translate(300, 100).attr({
+    text: {
+        text: 'Obstacle',
+        fill: '#eee'
+    }
+});
+
+var obstacles = [
+    obstacle,
+    obstacle.clone().translate(200, 100),
+    obstacle.clone().translate(-200, 150)
+];
+
+DEFAULT_GRAPH.addCells(obstacles).addCells([source, target, link]);
+
+link.toBack();
+
+DEFAULT_GRAPH.on('change:position', function(cell) {
+    // has an obstacle been moved? Then reroute the link.
+    for (var i = 0, tot = linklist.length; i < tot; i++) paper.findViewByModel(linklist[i]).update();
+});
+$('.router-switch').on('click', function(evt) {
+
+    var router = $(evt.target).data('router');
+    var connector = $(evt.target).data('connector');
+
+    if (router) {
+        link.set('router', {name: router});
+    } else {
+        link.unset('router');
+    }
+
+    link.set('connector', {name: connector});
+});
+$('#add_butt').click(function() {
+
+    var newNode = new joint.shapes.devs.Coupled({
+        position: {x: 500, y: 300},
+        size: {width: 140, height: 70},
+        inPorts: ['In'],
+        outPorts: ['Out'],
+        attrs: {
+            '.label': {text: $("#add_select").val()},
+            rect: {fill: '#d9534f'},
+
+            '.inPorts circle': {
+                fill: '#16A085',
+                magnet: 'passive',
+                type: 'input',
+                r: '10'
+            },
+            '.outPorts circle': {fill: '#E74C3C', type: 'output', r: '10'}
+        }
+    });
+    DEFAULT_GRAPH.addCell(newNode);
+});
+
 
 // allows for drag and drop
 $(document).ready(function() {
@@ -19,14 +145,14 @@ $(document).ready(function() {
         helper: 'clone',
         zIndex: 10000 // was having issues with paper being over draggable item
     });
-    $(document).ready(function(){
+    $(document).ready(function() {
         $('[data-toggle="popover"]').popover();
     });
     $("#paper").droppable({
 
         // creates a node on the mouse up,based on what image id was dropped
         // in the paper from the sidescroll list above
-        drop: function (event, ui) {
+        drop: function(event, ui) {
 
             var offset = $(this).offset();
             var relX = ui.position.left - offset.left;
@@ -35,7 +161,7 @@ $(document).ready(function() {
             var id = ui.draggable.prop('id');
 
             console.log($("#paper").css("z-index"));
-            console.log($("#"+id).css("z-index"));
+            console.log($("#" + id).css("z-index"));
 
             var dropText = '';
 
@@ -49,16 +175,25 @@ $(document).ready(function() {
             else dropText = 'CalculatorDivide';
 
             var newNode = new joint.shapes.devs.Coupled({
-                position: { x: relX, y: relY },
-                size: { width: 140, height: 70 },
-                inPorts : ['In'],
+                position: {x: relX, y: relY},
+                size: {width: 140, height: 70},
+                inPorts: ['In'],
                 outPorts: ['Out'],
                 attrs: {
-                    '.label'            : { text: dropText},
-                    rect                : { fill: '#d9534f' },
+                    '.label': {text: dropText},
+                    rect: {fill: '#d9534f'},
 
-                    '.inPorts circle'   : { fill: '#16A085', magnet: 'passive', type: 'input',r:'10' },
-                    '.outPorts circle'  : { fill: '#E74C3C', type: 'output',r:'10' }
+                    '.inPorts circle': {
+                        fill: '#16A085',
+                        magnet: 'passive',
+                        type: 'input',
+                        r: '10'
+                    },
+                    '.outPorts circle': {
+                        fill: '#E74C3C',
+                        type: 'output',
+                        r: '10'
+                    }
                 }
             });
             DEFAULT_GRAPH.addCell(newNode);
