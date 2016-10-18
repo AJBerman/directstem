3962f7a1a93644d9ae00648e042b784ea12f6f87
shay <shay.nguyen@gmail.com>
2016-10-18 07:30:11 -0700

    replace run-btn directives with a regular btn
    i was being too cute, it add complexity to  simple problem

--- app/features/project/info/summaryController.js
+++ app/features/project/info/summaryController.js
@@ -3,15 +3,33 @@
  */
 
 angular.module("WebserviceApp.Controllers")
-    .controller("InfoSummaryCtrl", function ($scope, ProjectFactory) {
+
+    .constant("SPIN_ICON", "fa-spin")
+
+    .constant("DEFAULT_BTN_CSS", "btn-default")
+
+    .constant("CANCEL_BTN_CSS", "btn-danger")
+
+    .controller("InfoSummaryCtrl", function($scope, ProjectFactory, SPIN_ICON,
+                                            DEFAULT_BTN_CSS, CANCEL_BTN_CSS) {
+
+        var runBtnToggle = false;
+
+        // default text, can be toggle to 'Stop Project"
+        var runBtnText = "Run Project";
+
         $scope.activeProject = ProjectFactory.getActiveProject();
 
-        $scope.getActiveProject = function () {
+        /* =============== PROJECT FUNCTIONS =============== */
+
+        $scope.getActiveProject = function() {
             $scope.activeProject = ProjectFactory.getActiveProject();
             return $scope.activeProject;
         };
 
-        $scope.saveGraph= function() {
+        /* =============== GRAPH FUNCTIONS =============== */
+
+        $scope.saveGraph = function() {
             ProjectFactory.saveGraph();
         };
 
@@ -21,5 +39,28 @@ angular.module("WebserviceApp.Controllers")
 
         $scope.loadGraph = function() {
             ProjectFactory.loadGraph();
+        };
+
+        /* =============== BUTTONS FUNCTIONS =============== */
+
+        $scope.toggleRunBtn = function() {
+            runBtnToggle = !runBtnToggle;
+            runBtnToggle ? runBtnText = "Stop Project" : runBtnText = "Run Project";
+        };
+
+        $scope.getIconClass = function() {
+            console.log("getIconClass called");
+            return runBtnToggle ? SPIN_ICON : ""
+        };
+
+        // display the "run" attribute text as the default text (when
+        // the page is first loaded, else return the text field .
+        $scope.displayBtnText = function() {
+            return runBtnText;
+        };
+
+        $scope.getBtnClass = function() {
+            return runBtnToggle ? CANCEL_BTN_CSS : DEFAULT_BTN_CSS;
         }
     });
+
