9f94966c3e915d171540314333afa8c233e8fd4c
shay <shay.nguyen@gmail.com>
2016-10-18 09:21:07 -0700

    Replace the example graph with a real graph

--- app/features/graph/joint_setup.js
+++ app/features/graph/joint_setup.js
@@ -12,65 +12,56 @@ var DEFAULT_GRAPH = new joint.dia.Graph();
 
 /* =============== CREATING THE DEFAULT GRAPH =============== */
 
-var member = function(x, y, rank, name, image, background, textColor) {
-    textColor = textColor || "#000";
+// allows for drag and drop
+$(document).ready(function() {
 
-    var cell = new joint.shapes.org.Member({
-        position: {x: x, y: y},
-        attrs: {
-            '.card': {fill: background, stroke: 'none'},
-            image: {'xlink:href': 'images/' + image, opacity: 0.7},
-            '.rank': {
-                text: rank,
-                fill: textColor,
-                'word-spacing': '-5px',
-                'letter-spacing': 0
-            },
-            '.name': {
-                text: name,
-                fill: textColor,
-                'font-size': 13,
-                'font-family': 'Arial',
-                'letter-spacing': 0
-            }
+    $(".item").draggable({
+        helper: 'clone',
+        zIndex: 10000 // was having issues with paper being over draggable item
+    });
+    $(document).ready(function(){
+        $('[data-toggle="popover"]').popover();
+    });
+    $("#paper").droppable({
+
+        // creates a node on the mouse up,based on what image id was dropped
+        // in the paper from the sidescroll list above
+        drop: function (event, ui) {
+
+            var offset = $(this).offset();
+            var relX = ui.position.left - offset.left;
+            var relY = ui.position.top - offset.top;
+
+            var id = ui.draggable.prop('id');
+
+            console.log($("#paper").css("z-index"));
+            console.log($("#"+id).css("z-index"));
+
+            var dropText = '';
+
+            // id will determine what type of node is created
+            // color is used for testing
+            if (id == 'img1') dropText = 'WeatherByZip';
+            else if (id == 'img2') dropText = 'WeatherByCity';
+            else if (id == 'img3') dropText = 'CalculatorAdd';
+            else if (id == 'img4') dropText = 'CalculatorSubtract';
+            else if (id == 'img5') dropText = 'CalculatorMultiply';
+            else dropText = 'CalculatorDivide';
+
+            var newNode = new joint.shapes.devs.Coupled({
+                position: { x: relX, y: relY },
+                size: { width: 140, height: 70 },
+                inPorts : ['In'],
+                outPorts: ['Out'],
+                attrs: {
+                    '.label'            : { text: dropText},
+                    rect                : { fill: '#d9534f' },
+
+                    '.inPorts circle'   : { fill: '#16A085', magnet: 'passive', type: 'input',r:'10' },
+                    '.outPorts circle'  : { fill: '#E74C3C', type: 'output',r:'10' }
+                }
+            });
+            DEFAULT_GRAPH.addCell(newNode);
         }
     });
-    DEFAULT_GRAPH.addCell(cell);
-    return cell;
-};
-
-function link(source, target, breakpoints) {
-
-    var cell = new joint.shapes.org.Arrow({
-        source: {id: source.id},
-        target: {id: target.id},
-        vertices: breakpoints,
-        attrs: {
-            '.connection': {
-                'fill': 'none',
-                'stroke-linejoin': 'round',
-                'stroke-width': '2',
-                'stroke': '#4b4a67'
-            }
-        }
-
-    });
-    DEFAULT_GRAPH.addCell(cell);
-    return cell;
-}
-
-var bart = member(300, 70, 'CEO', 'Bart Simpson', 'male.png', '#30d0c6');
-var homer = member(90, 200, 'VP Marketing', 'Homer Simpson', 'male.png', '#7c68fd', '#f1f1f1');
-var marge = member(300, 200, 'VP Sales', 'Marge Simpson', 'female.png', '#7c68fd', '#f1f1f1');
-var lisa = member(500, 200, 'VP Production', 'Lisa Simpson', 'female.png', '#7c68fd', '#f1f1f1');
-var maggie = member(400, 350, 'Manager', 'Maggie Simpson', 'female.png', '#feb563');
-var lenny = member(190, 350, 'Manager', 'Lenny Leonard', 'male.png', '#feb563');
-var carl = member(190, 500, 'Manager', 'Carl Carlson', 'male.png', '#feb563');
-
-
-link(bart, marge, [{x: 385, y: 180}]);
-link(bart, homer, [{x: 385, y: 180}, {x: 175, y: 180}]);
-link(bart, lisa, [{x: 385, y: 180}, {x: 585, y: 180}]);
-link(homer, lenny, [{x: 175, y: 380}]);
-link(homer, carl, [{x: 175, y: 530}]);
-link(marge, maggie, [{x: 385, y: 380}]);
+});
