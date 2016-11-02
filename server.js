5cb2085b3849e3156d56f3c9930755492d0c722d
shay <shay.nguyen@gmail.com>
2016-11-02 00:35:17 -0700

    Impl a basic webservice: count letter frequency

--- server.js
+++ server.js
@@ -6,12 +6,43 @@
 
 const express = require("express");
 const app = new express();
+const bodyParser = require("body-parser");
 
 app.set("port", process.env.port || 3000);
 
 app.use(express.static("app"));
 app.use(express.static("app/styles"));
 
+
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+app.set("port", 3000);
+
+app.use(express.static("app"));
+
+
+app.post("/api/letters", jsonParser, function (req, res) {
+
+    function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    var chart = {};
+    var max = Math.floor(Math.random() * (500- 50 + 1) + 50);
+
+    chart.data = [
+        {letter: "W", count: randomNumber(1, max)},
+        {letter: "E", count: randomNumber(1, max)},
+        {letter: "S", count: randomNumber(1, max)},
+        {letter: "T", count: randomNumber(1, max)},
+        {letter: "O", count: randomNumber(1, max)},
+        {letter: "R", count: randomNumber(1, max)},
+        {letter: "L", count: randomNumber(1, max)},
+        {letter: "D", count: randomNumber(1, max)}
+    ];
+    res.send(chart);
+});
 app.use((req, res) => {
     res.status(404);
     res.send("404 - Not Found");
